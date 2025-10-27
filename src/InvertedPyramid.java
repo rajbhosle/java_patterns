ad2be867af8bdfe8f93bce0a0eba1f289e7ff444
Raj <bhosleraj252@gmail.com>
2025-10-27 13:47:39 +0530

    Inverted Pyramid pattern

--- src/InvertedPyramid.java
+++ src/InvertedPyramid.java
@@ -1,12 +1,12 @@
 public class InvertedPyramid {
     public static void main(String[] args) {
-            for(int i = 5; i >= 1; i--) {
-                for(int j = i; j < 5; j++)
-                    System.out.print(" ");
-                for(int k = 1; k <= (2*i - 1); k++)
-                    System.out.print("*");
-                System.out.println();
-            }
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j < 5; j++)
+                System.out.print(" ");
+            for (int k = 1; k <= (2 * i - 1); k++)
+                System.out.print("*");
+            System.out.println();
         }
     }
+}
 
